10a735b0304791e316996c0a68cb5f312e964a0a
marineB210 <marineop93@gmail.com>
2024-06-09 18:33:44 +0200

    Add support to distance on website

--- cloud_web/views.py
+++ cloud_web/views.py
@@ -52,9 +52,10 @@ def search(request):
         query_image = request.POST.get('query-image', '')
         top = request.POST.get('top', '')
         descriptors = request.POST.getlist('descriptor')
-        if query_image and top and descriptors:
+        distance = request.POST.get('metric', '')
+        if query_image and top and descriptors and distance:
             descriptors_str = ','.join(descriptors)
-            return redirect(f"{reverse('results')}?query-image={query_image}&top={top}&descriptors={descriptors_str}")
+            return redirect(f"{reverse('results')}?query-image={query_image}&top={top}&descriptors={descriptors_str}&distance={distance}")
         else:
             return render(request, 'search.html', {'error': 'Please fill in all fields.'})
     return render(request, 'search.html')
@@ -65,13 +66,14 @@ def results(request):
     query_image = request.GET.get('query-image', '')
     top = int(request.GET.get('top', ''))
     descriptors = request.GET.get('descriptors', '').split(',')
+    distance = int(request.GET.get('distance', ''))
     print(f'Query image: {query_image}')  # Debug print statement
     print(f'Top: {top}')  # Debug print statement
     print(f'Descriptors: {descriptors}')  # Debug print statement
 
     if query_image and top and descriptors:
         # Process the data here
-        query_image_name, close_image_paths, close_image_names = search_with_filename(query_image, top, descriptors[0])
+        query_image_name, close_image_paths, close_image_names = search_with_filename(query_image, top, descriptors[0], distance)
         results_images = []
         for path in close_image_paths:
             results_images.append(os.path.join(settings.MEDIA_URL, path))
@@ -104,20 +106,44 @@ def euclidean_distance(l1: np.ndarray, l2: np.ndarray) -> float:
     return np.linalg.norm(l1 - l2)
 
 
-def get_k_nearest_neighbors(features: List[Tuple[str, np.ndarray]], test: Tuple[str, np.ndarray], k: int) -> List[
-    Tuple[str, np.ndarray, float]]:
-    distances = [(name, feat, euclidean_distance(test[1], feat)) for name, feat in features]
+def chi_square_distance(l1: np.ndarray, l2: np.ndarray) -> float:
+    epsilon = 1e-10
+    l2_safe = np.where(l2 == 0, epsilon, l2)
+
+    return np.sum((l1 - l2) ** 2 / l2_safe)
+
+
+def bhattacharyya_distance(l1: np.ndarray, l2: np.ndarray) -> float:
+    N_1 = np.mean(l1)
+    N_2 = np.mean(l2)
+
+    score = np.sum(np.sqrt(l1 * l2))
+
+    den = np.sqrt(N_1 * N_2 * len(l1) * len(l2))
+
+    distance = np.sqrt(1 - (score / den))
+
+    return distance
+
+def get_k_nearest_neighbors(features: List[Tuple[str, np.ndarray]], test: Tuple[str, np.ndarray], k: int, distance_function: int) -> List[Tuple[str, np.ndarray, float]]:
+    distances = []
+    if distance_function == 0:
+        distances = [(name, feat, euclidean_distance(test[1], feat)) for name, feat in features]
+    elif distance_function == 1:
+        distances = [(name, feat, chi_square_distance(test[1], feat)) for name, feat in features]
+    elif distance_function == 2:
+        distances = [(name, feat, bhattacharyya_distance(test[1], feat)) for name, feat in features]
     distances.sort(key=lambda x: x[2])
     return distances[:k]
 
-def search_with_filename(filename: str, top: int, descriptor: str) -> Tuple[str, List[str], List[str]]:
+def search_with_filename(filename: str, top: int, descriptor: str, distance_function: int) -> Tuple[str, List[str], List[str]]:
     features = load_features_from_json(f"Features_train/{descriptor}.json")
     image_req = next(i for i, (path, _) in enumerate(features) if os.path.basename(path) == filename)
-    return search_images(image_req, top, descriptor)
+    return search_images(image_req, top, descriptor, distance_function)
 
-def search_images(image_req: int, top: int, descriptor: str) -> Tuple[str, List[str], List[str]]:
+def search_images(image_req: int, top: int, descriptor: str, distance_function: int) -> Tuple[str, List[str], List[str]]:
     features = load_features_from_json(f"Features_train/{descriptor}.json")
-    neighbors = get_k_nearest_neighbors(features, features[image_req], top)
+    neighbors = get_k_nearest_neighbors(features, features[image_req], top, distance_function)
 
     query_image_path = features[image_req][0]
     query_image_name = os.path.splitext(os.path.basename(query_image_path))[0]
